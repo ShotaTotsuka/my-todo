731d12fc45dc1126bf016aa63a0a68c194103dbe
ShotaTotsuka <shota.totsuka@blueish.co.jp>
2024-01-26 21:15:21 +0900

    add post method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::net::SocketAddr;
-use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -14,4 +29,25 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+// POSTはJson型?を受け取る
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)] // 構造体に変換できる
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)] // JSONに変換できる
+struct User {
+    id: u64,
+    username: String,
 }
